f5944fc5d35b9e1c3dd659fbcf26f84a62a1911a
zanz1n <izanrodrigues999@gmail.com>
2022-03-25 18:15:26 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -56,53 +56,31 @@ client.on("messageCreate", (message) => {
     if (message.author.bot) return
 
     if (message.content === prefix + help.name) help.execute(message)
-
     else if (message.content === prefix + piada.name) piada.execute(message)
-
     else if (message.content === prefix + pedro.name) pedro.execute(message)
-
     else if (message.content === prefix + bruno.name) bruno.execute(message)
-
     else if (message.content === prefix + penis.name) penis.execute(message)
-
     else if (message.content === prefix + sus.name) sus.execute(message)
-
     else if (message.content === prefix + tutao.name) tutao.execute(message)
 
     else if (message.content === `${prefix}angola`) { message.channel.send('Onde o bruno mora') }
-
     else if (message.content === `${prefix}gay`) { message.channel.send('Não pode gay no servidor, desculpe') }
-
     else if (message.content === `${prefix}gabriel`) { message.channel.send('Para com isso Gabriel caralho') }
-
     else if (message.content === `${prefix}lopinho`) { message.channel.send('Namorou 6 meses no LOL e era homem') }
-
     else if (message.content === `${prefix}hindrigo`) { message.channel.send('Gay, femboy, nunca online.\nSe tiver online, espanca o teclado.') }
-
     else if (message.content === `${prefix}luigi`) { message.channel.send('Cheira queijo e faz tique toque (cheira pó)') }
-
     else if (message.content === `${prefix}homossexual`) { message.channel.send('Palavra chique pra gay, tambem nao pode aqui!') }
-
     else if (message.content === `${prefix}izan`) { message.channel.send('O cara mais gostoso e criador desse bot') }
-
     else if (message.content === `${prefix}creepin`) { message.channel.send('Outro semideus que ajudou na criacao do BOT (tem alseimer)') }
-
     else if (message.content === `${prefix}deus`) { message.channel.send('Não existe, mas aqui no server é o Izan') }
-
     else if (message.content === `${prefix}guizin`) { message.channel.send('Ta dormindo ate hoje') }
-
     else if (message.content === `${prefix}gustavo`) { message.channel.send('Tijolo') }
-
     else if (message.content === `${prefix}genshinimpact`) { message.reply('É crime, vc vai ser banido se falar denovo') }
-
     else if (message.content === `${prefix}denis`) { message.channel.send('╭ᑎ╮') }
-
     else if (message.content === `${prefix}hel`) { message.reply('Escreve direito imbecil é Help') }
-
     else if (message.content === `${prefix}piplup`) {
         message.channel.send('https://cdn.discordapp.com/attachments/954933541051985920/955186822470836244/MicrosoftTmghfghmgeams-image.png')
     }
-
     else if (message.content.startsWith(prefix)) { message.reply('Isso não é um comando, escreve direito primata') }
 })
 
