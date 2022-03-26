4cbf7b030ec091b82132d2d66aed6004b3462249
zanz1n <izanrodrigues999@gmail.com>
2022-03-26 10:03:49 -0300

    changing the shellscript

--- download.sh
+++ download.sh
@@ -1,4 +1,8 @@
-cd src
+wget https://github.com/zanz1n/kid-bengala-bot/archive/refs/heads/main.zip &&
+    unzip main.zip &&
+    rm main.zip &&
+    cd kid-bengala-bot-main/src
+
 read -p "Are you sure that you want to deploy that container?" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     sudo docker build -t kid-bengala/dockernode . && sudo docker run --rm -d kid-bengala/dockernode
