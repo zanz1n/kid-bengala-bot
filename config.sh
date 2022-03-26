d1ded3d4bc5413f40a643ea6aa0bcb7c4864212c
zanz1n <izanrodrigues999@gmail.com>
2022-03-26 10:00:19 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-unzip main.zip && cd src
+unzip main.zip && rm main.zip && cd src
 read -p "Are you sure that you want to deploy that container?" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     sudo docker build -t kid-bengala/dockernode . && sudo docker run --rm -d kid-bengala/dockernode
