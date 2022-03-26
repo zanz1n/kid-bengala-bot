68188ebe1ef054e8ea36c570f6c508e0ceefd5bd
zanz1n <izanrodrigues999@gmail.com>
2022-03-26 10:11:21 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,9 @@ wget https://github.com/zanz1n/kid-bengala-bot/archive/refs/heads/main.zip &&
     rm main.zip &&
     cd kid-bengala-bot-main/src
 
-read -p "Are you sure that you want to deploy that container?" -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+read -p "Are you sure that you want to deploy that container?(y/n)" CONT
+if [[ ! $CONT = "y" ]]; then
     sudo docker build -t kid-bengala/dockernode . && sudo docker run --rm -d kid-bengala/dockernode
+else
+    echo "You will need to run the aplication later, otherwhise will not work"
 fi
