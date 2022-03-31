28840df9fd5e34c2d1b3fdb123e728d9ff304957
zanz1n <izanrodrigues999@gmail.com>
2022-03-31 18:33:49 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -56,10 +56,19 @@ client.on("interactionCreate", async (interaction) => {
     api.mem()
     try {
         if (!interaction.isCommand()) return; if (interaction.user.bot) return
-
         else if (interaction.commandName === slashhelp.name) slashhelp.execute(interaction)
         else if (interaction.commandName === slashavatar.name) slashavatar.execute(interaction)
-        else if (interaction.commandName === slashembed.name) slashembed.execute(interaction)
+        else if (interaction.commandName === slashembed.name) {
+            const options = interaction.options._hoistedOptions
+            const user = (options.find((e) => e.name === "user") && options.find((e) => e.name === "user").member.user) || interaction.user
+            const member = (options.find((e) => e.name === "user") && options.find((e) => e.name === "user").memebr) || interaction.member
+
+            const embed = new MessageEmbed().setColor(member.displayHexColor)
+            const image = user.displayAvatarURL({ dynamic: true, size: 512 })
+
+            embed.setAuthor(user.username, user.displayAvatarURL()).setImage(image).setFooter({ text: `Requisitado por: ${interaction.user.username}`, iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.webp` }).setTimestamp()
+            interaction.reply({ embeds: [embed] }) //await
+        }
     } catch (err) {
         api.err('Something went wrong with the slashCommand help')
     }
